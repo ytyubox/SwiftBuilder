e71fb1356cf9e436413b3c9aeb964c9c64e0aac8
Tsungyu Yu <ytyubox@gmail.com>
2020-03-03 15:28:48 +0800

    add test case

---
+++ Tests/FluentInterfaceTests/FluentinterfaceHandlerTests.swift
@@ -0,0 +1,36 @@
+import FluentInterface
+import XCTest
+
+class FluentinterfaceHandlerTests: XCTestCase {
+	
+	func testHandlingSubject() {
+		class TestTarget {
+			var int:Int = 1
+			func doubleIt() {
+				int *= 2
+			}
+		}
+		let target = TestTarget()+
+			.handlingSubject{$0.doubleIt()}
+		.unwrappingSubject()
+		
+		XCTAssertEqual(target.int, 2)
+	}
+	func testManipulateSubjct() {
+		struct TestTarget {
+			var int:Int = 1
+			mutating func doubleIt() {
+				int *= 2
+			}
+		}
+		let target = TestTarget()+
+			.manipulateSubjct{$0.doubleIt()}
+		.unwrappingSubject()
+		
+		XCTAssertEqual(target.int, 2)
+	}
+	static var allTests = [
+		("testHandlingSubject", testHandlingSubject),
+		("testManipulateSubjct",testManipulateSubjct),
+	]
+}
